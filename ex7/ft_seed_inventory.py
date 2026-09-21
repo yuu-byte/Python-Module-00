4adecf29e9811e65481c8151aa4757230da72d27
Yu Ito <271247412+yuu-byte@users.noreply.github.com>
2026-09-22 02:54:14 +0900

    ex7を追加

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,14 +1,22 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ft_hello_garden.py                                 :+:      :+:    :+:    #
+#    ft_seed_inventory.py                               :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: yuito <yuito@student.42tokyo.jp>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/09/13 16:59:21 by yuito             #+#    #+#              #
-#    Updated: 2026/09/22 02:53:47 by yuito            ###   ########.fr        #
+#    Created: 2026/09/22 01:01:18 by yuito             #+#    #+#              #
+#    Updated: 2026/09/22 02:44:00 by yuito            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def ft_hello_garden():
-	print("Hello, Garden Community!")
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+	display_name = seed_type.capitalize()
+	if unit == "packets":
+		print(f"{display_name} seeds: {quantity} packets available")
+	elif unit == "grams":
+		print(f"{display_name} seeds: {quantity} grams total")
+	elif unit == "area":
+		print(f"{display_name} seeds: covers {quantity} square meters")
+	else:
+		print("Unknown unit type")
